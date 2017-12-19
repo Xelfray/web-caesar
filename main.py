2ec4cc8fdce2b1da2798eb43eb10ad65c3d91284
Raimir Alvarez <rah93cub@gmail.com>
2017-12-19 14:41:55 -0500

    master branch done

--- main.py
+++ main.py
@@ -4,34 +4,64 @@ from caesar import rotate_string
 app=Flask(__name__)
 app.config['DEBUG']=True
 
-form="""
-<!DOCTYPE html>
+# form="""
+# <!DOCTYPE html>
+# <html>
+#     <head>
+#         <style> form {{background-color: #eee;
+#                 padding: 20px;
+#                 margin: 0 auto;
+#                 width: 540px;
+#                 font: 16px sans-serif;
+#                 border-radius: 10px;
+#             }}
 
+#             textarea {{
+#                 margin: 10px 0;
+#                 width: 540px;
+#                 height: 120px;
+#             }}
+#         </style>
+#         <title>Web-Caesar</title>
+#     </head>
+#     <body>
+#         <form method="POST">
+#             <label>Rotate by:
+#                 <input type="text" name="rot" value='0' />
+#             </label></br>
+#             <textarea  name='text' value=''>{0}</textarea>
+#             <input type="submit" />
+#         </form>
+#     </body>
+# </html>
+# """
+
+form = """
+<!DOCTYPE html>
 <html>
     <head>
         <style>
-            form{
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea{
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
-        <title>Web-Caesar</title>
     </head>
     <body>
-        <form method="POST">
+      <form method="POST">
             <label>Rotate by:
-                <input type="text" name="rot" value='0' />
+                <input type="text" name="rot" value={0} />
             </label></br>
-            <textarea  name='text' value='{0}'></textarea>
+            <textarea  name='text'>{1}</textarea>
             <input type="submit" />
         </form>
     </body>
@@ -40,19 +70,14 @@ form="""
 
 @app.route('/')
 def index():
-    return form
-
+    return form.format(0, '')
 
 @app.route('/', methods=["POST"])
 def encrypt():
-    
     rot= int(request.form['rot'])
     text= request.form['text']
     encrypted_text=rotate_string(text, rot)
-    
-    return (form.format(encrypted_text))
-    #return encrypted_text
-    
 
-
+    return form.format(rot, encrypted_text)
+    
 app.run()
